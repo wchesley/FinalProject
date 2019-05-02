ed421a671711cd3d7a1b1b54b854ab2c4fad6008
wchesley <wrchesley1@buffs.wtamu.edu>
2019-05-02 14:06:29 -0500

    hotfix on speakerbio page

--- components/SpeakerBio.js
+++ components/SpeakerBio.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, Switch, StyleSheet, Image, FlatList } from 'react-native'
-import { ListItem, Button, Icon, Card } from 'react-native-elements'
+import { ListItem, Button, Icon, Tile5, Tile } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler'
 import babb from './img/babb.png'
 import { connect } from 'react-redux'
@@ -9,6 +9,11 @@ import firebase from 'react-native-firebase'
 import { withNavigation } from 'react-navigation'
 
 
+/* 
+    TODO: 
+    1. Navigation without props: https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html
+    2. Read individual doc from firebase; 
+*/
 const mapStateToProps = (state) => {
     return {
         speakerData: state.speakerData
@@ -25,6 +30,7 @@ class SpeakerBio extends React.Component {
 
     constructor() {
         super();
+        //page won't render with these enabled: ref TODO #1 Returns: TypeError: Undefined is not an object (evaluating '_this.props.navigation')
         //const { navigation } = this.props; 
         //const speakerCollection = navigation.getParam(BioRef, 'Speakers/AmjatAbudllat');
         this.ref = firebase.firestore().collection('Speakers');
@@ -71,13 +77,15 @@ class SpeakerBio extends React.Component {
         }
         return (
             <View>
-                <Card
-                    title={this.state.speakerData.Name}
-                    image={require('./img/babb.png')}>
+                <Tile
+                imageSrc={require('./img/babb.png')}
+                title="Jeff Babb"
+                contentContainerStyle={{height:120}}
+                >
                     <Text style={{ marginBottom: 10 }}>
-                        {this.state.speakerData.Description}
+                    Dr. Babb has extensive experience in mentoring and coaching students in software design competitions, including coaching two winning teams in the national championship for Microsoft’s Imagine Cup Software Design Invitational.
                     </Text>
-                </Card>
+                </Tile>
             </View>
         );
     }
